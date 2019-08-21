5840f5250c899d2633438cffdbe48123b5c6f1de
0xDobharchu <doanh@autonomous.nyc>
2019-08-21 10:45:00 +0700

    add constant star token

---
+++ Ethereum/contracts/Consta.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.5;
+
+import 'https://github.com/constant-money/constant-smart-contract/Ethereum/contracts/Admin.sol';
+import 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+
+contract Constant is ERC20, Admin {
+
+        // token info
+        string public constant name = "Constant star";
+        string public constant symbol = "CONSTA";
+        uint public constant decimals = 2;
+
+        // events to track onchain-offchain relationships
+        event __transferByAdmin(bytes32 offchain);
+        event __purchase(bytes32 offchain);
+        event __redeem(bytes32 offchain);
+
+        /**
+         * @dev function to transfer CONS
+         * @param from the address to transfer from
+         * @param to the address to transfer to
+         * @param value the amount to be transferred
+         */
+        function transferByAdmin(
+                address from,
+                address to,
+                uint value,
+                bytes32 offchain
+        )
+                public
+                onlyAdmin
+        {
+                _transfer(from, to, value);
+                emit __transferByAdmin(offchain);
+        }
+
+
+        /**
+         * @dev function to purchase new CONST
+         * @param purchaser the address that will receive the newly minted CONST
+         * @param value the amount of CONST to mint
+         */
+        function purchase(
+                address purchaser,
+                uint value,
+                bytes32 offchain
+        )
+                public
+                onlyAdmin
+        {
+                _mint(purchaser, value);
+                emit __purchase(offchain);
+        }
+
+
+        /**
+         * @dev function to burn CONST
+         * @param redeemer the account whose CONST will be burnt
+         * @param value the amount of CONST to be burnt
+         */
+        function redeem(
+                address redeemer,
+                uint value,
+                bytes32 offchain
+        )
+                public
+                onlyAdmin
+        {
+                _burn(redeemer, value);
+                emit __redeem(offchain);
+        }
+}
